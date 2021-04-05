088d3217b8c2a6e45d690b2b3f84cd1c7505d179
Gabriel Wolf <gabeericwolf@gmail.com>
2021-04-05 16:06:24 -0400

    yep

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector(".btn-toggle");
+        const theme = document.querySelector("#theme-link");
+        btn.addEventListener("click", function() {
+          // Swap out the URL for the different stylesheets
+          if (theme.getAttribute("href") == "css/light-theme.css") {
+            theme.href = "css/dark-theme.css";
+          } else {
+            theme.href = "css/light-theme.css";
+          }
+        });
