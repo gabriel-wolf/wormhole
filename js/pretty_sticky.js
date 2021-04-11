3c96b5805ed31c6a0fbf1a7efc5cce880ee1294d
Gabriel Wolf <gabeericwolf@gmail.com>
2021-04-10 21:37:06 -0400

    FINALLY

--- js/pretty_sticky.js
+++ js/pretty_sticky.js
@@ -1,7 +1,14 @@
-        
-function book(t, name) {
+
+function book(t, name, which = "title") {
     var el = t[name];
-    var toout = "<a href='" + t[name][0] + "'>" + name + "</a>"
+    var auth = t[name][1];
+    console.log(el)
+    var toout = "";
+    if (which == "author") {
+        toout = "<a href='" + t[name][0] + "'>" + auth + "</a>"
+    } else {
+        toout = "<a href='" + t[name][0] + "'>" + name + "</a>"
+    }
     return toout;
 }
 
@@ -10,96 +17,101 @@ function swapall(t) {
     var a = document.getElementsByClassName("swapme");
     var i;
 
-    for (i=0;i<a.length;i++) {
-        var x = a[i];
-        // x.style.color="red";
-        var cont = x.innerHTML;
-        var ct = t[cont];
+    for (i = 0; i < a.length; i++) {
+        try {
 
-        // t['term'] = [tit,bod,srcs,autit,useimg,imgloc,imgcapa,imgsrc,imgcapb]
 
-        if (t[0] == "") {
-            
-        } if (t[1] == "") {
-            
-        } if (t[2] == "") {
-            
-        } if (t[3] == "") {
-            
-        } if (t[4] == "") {
-            
-        } if (t[5] == "") {
-            
-        } if (t[6] == "") {
-            
-        } if (t[7] == "") {
-            
-        } if (t[8] == "") {
-            
+            var x = a[i];
+            // x.style.color="red";
+            var cont = x.innerHTML;
+            var ct = t[cont];
+
+            // t['term'] = [tit,bod,srcs,autit,useimg,imgloc,imgcapa,imgsrc,imgcapb]
+
+            var TIT = ct[0];
+            var BOD = ct[1];
+            var SOURCELIST = ct[2];
+            var AUTID = ct[3];
+            var USEIMG = ct[4];
+            var IMGLOC = ct[5];
+            var IMGCAPA = ct[6];
+            var IMGSRC = ct[7];
+            var IMGCAPB = ct[8];
+
+            var TERM = cont;
+            if (ct[0] == "") {
+                TIT = "Wormhole";
+            } if (ct[1] == "") {
+                BOD = "A wormhole (also known as Einstein–Rosen bridge) is a passage through spacetime creating shortcuts for long journeys across the universe.";
+            } if (ct[2] == "") {
+                SOURCELIST = [
+                    "The Science of Interstellar",
+                    "The Birth of Wormholes"
+                ];
+            } if (ct[3] == "") {
+                AUTID = "title";
+            } if (ct[4] == "") {
+                USEIMG = false;
+            } if (ct[5] == "") {
+                IMGLOC = "'imgs/hole01.jpg'";
+            } if (ct[6] == "") {
+                IMGCAPA = "Image from: ";
+            } if (ct[7] == "") {
+                IMGSRC = "The Science of Interstellar";
+            } if (ct[8] == "") {
+                IMGCAPB = ".";
+            }
+
+
+
+
+            var l1 = "<span onclick='void(0);' class='tooltip'>";
+            var l2 = "<span class='info'><span class='pronounce'>";
+            var l3 = "</span><span class='text'>";
+            var l3a = "</span>";
+            var l4IMG = "<img class='def-img' style='width:inherit;height:auto;' src=";
+            var l5IMG = "><span class='img-cap-span'>";
+            var l5a = "</span>";
+            var allIMG = "";
+            if (USEIMG == true) {
+                allIMG = l4IMG + IMGLOC + l5IMG + IMGCAPA + book(t, IMGSRC) + IMGCAPB + l5a;
+            } else {
+                allIMG = "";
+            }
+            var l6 = "<span class='bottom-span grid x-small'>";
+            var SOURCESTEXT = "";
+            var j;
+            for (j = 0; j < SOURCELIST.length; j++) {
+                SOURCESTEXT = SOURCESTEXT + book(t, SOURCELIST[j], AUTID);
+            }
+            var l7 = "</span></span></span>";
+
+            console.log(SOURCESTEXT);
+
+            // l1 + TERM + l2 + TIT + l3 + BOD + l3a + allIMG + l6 + SOURCESTEXT + l7
+
+            var final = l1 + TERM + l2 + TIT + l3 + BOD + l3a + allIMG + l6 + SOURCESTEXT + l7;
+
+            console.log(final);
+
+            a[i].innerHTML = final;
+
+
+            // console.log(cont);
         }
-       
-
-
-var l1 = "<span onclick='void(0);' class='tooltip'>";
-var TERM = cont;
-var l2 = "<span class='info'><span class='pronounce'>";
-var TIT = "Wormhole";
-var l3 = "</span><span class='text'>";
-var BOD = "A wormhole (also known as Einstein–Rosen bridge) is a passage through spacetime creating shortcuts for long journeys across the universe.";
-var l3a = "</span>";
-
-var USEIMG = false;
-var allIMG = "";
-if (USEIMG == true) {
-    var l4IMG = "<img class='def-img' style='width:inherit;height:auto;' src=";
-    var IMGLOC = "'imgs/hole01.jpg'";
-    var l5IMG = "><span class='img-cap-span'>";
-    var IMGCAPA = "Image from: ";
-    var IMGSRC = "<a href='https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378'>The Science of Interstellar</a>";
-    var IMGCAPB = ".";
-    var l5a = "</span>";
-    allIMG = l4IMG + IMGLOC + l5IMG + IMGCAPA + IMGSRC + IMGCAPB + l5a;
-} else {
-    allIMG = "";
-}
-
-
-var l6 = "<span class='bottom-span grid x-small'>";
-var SOURCELIST = [
-    "The Science of Interstellar",
-    "The Birth of Wormholes"
-];
-var SOURCESTEXT = "";
-var j;
-for (j=0;j<SOURCELIST.length;j++) {
-    SOURCESTEXT = SOURCESTEXT + book(t, SOURCELIST[j]);
-}
-var l7 = "</span></span></span>";
-
-
-
-console.log(SOURCESTEXT);
-
-        // l1 + TERM + l2 + TIT + l3 + BOD + l3a + allIMG + l6 + SOURCESTEXT + l7
-
-
-        var final = l1 + TERM + l2 + TIT + l3 + BOD + l3a + allIMG + l6 + SOURCESTEXT + l7;
-
-        console.log(final);
-
-
-
-
-        // console.log(cont);
+        catch (err) {
+            console.log("error error error");
+            a[i].innerHTML = "<p style='color:red;'>[FAILD TO RETREIVE DATA]</p>";
+        }
     }
 }
 
 
 function switchme(ele, t) {
-// console.log("started switch");
+    // console.log("started switch");
 
     var id = ele.id;
-    
+
     var tit = t[0];
     var bod = t[1];
 
@@ -108,7 +120,7 @@ function switchme(ele, t) {
     var put_before = "<span id='killmebefore' onclick='void(0);' class='tooltip'>";
     var put_middle = "<a class='sticky'>wormholes baby</a>";
     var put_after = "<span class='info'><span class='pronounce'>Wormhole</span><span class='text'>A wormhole (also known as Einstein–Rosen bridge) is a passage through spacetime creating shortcuts for long journeys across the universe.</span><span class='img-cap-span'>Image of wormhole from <a href='https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378'>The Science of Interstellar</a>.</span><span class='bottom-span grid x-small'><a href='https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378'>The Science of Interstellar</a><a href='https://physics.aps.org/story/v15/st11'>The Birth of Wormholes</a></span></span></span>";
-    
+
     var elehtml = ele.outerHTML;
     var fullhtml = put_before + put_middle + put_after;
 
@@ -125,7 +137,7 @@ function switchme(ele, t) {
 }
 
 
-function alt(){
+function alt() {
     console.log("log sys test");
     alert("alert system test");
 }
@@ -148,7 +160,7 @@ function stm(ele, t) {
 
     var put_before = "<span id='killmebefore' onclick='void(0);' class='tooltip'>";
     var put_after = "<span class='info'><span class='pronounce'>Wormhole</span><span class='text'>A wormhole (also known as Einstein–Rosen bridge) is a passage through spacetime creating shortcuts for long journeys across the universe.</span><span class='img-cap-span'>Image of wormhole from <a href='https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378'>The Science of Interstellar</a>.</span><span class='bottom-span grid x-small'><a href='https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378'>The Science of Interstellar</a><a href='https://physics.aps.org/story/v15/st11'>The Birth of Wormholes</a></span></span></span>";
-    
+
     var elehtml = ele.outerHTML;
     var fullhtml = put_before + elehtml + put_after;
 
--- js/pretty_text.js
+++ js/pretty_text.js
@@ -1,11 +1,11 @@
-var FiltersEnabled = 1
+var FiltersEnabled = 1;
 
 // t['term'] = [tit,bod,srcs,autit,useimg,imgloc,imgcapa,imgsrc,imgcapb]
 
 Text['wormhole'] = [ // inline term
     "Wormhole", // title
     "Structure linking disparate points in spacetime.", // body definition
-    ["Gabe","Singularities"], // srcs 
+    ["Gabe","The Science of Interstellar"], // srcs 
     "author", // use "author" or "title"?
     true, // use image?
     "../imgs/wormhole01.jpg", 
@@ -16,22 +16,36 @@ Text['wormhole'] = [ // inline term
 
 Text['singularity'] = [
     "sing",
-    "these heeeya are amazing"
+    "these heeeya are amazing",
+    [""],
+    "title",
+    false,
+    "",
+    "",
+    "",
+    ""
 ]
 
 Text['waste'] = [
     "wasted",
-    "these waste waste are amazing"
+    "these waste waste are amazing",
+    [""],
+    "title",
+    false,
+    "",
+    "",
+    "",
+    ""
 ]
 
 
 
 
 
-Text['Gabe'] = ["https://www.help.hawken.edu"]
-Text['The Science of Interstellar'] = ["https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378"]
-Text['New Improved Wormholes'] = ["www.npl.washington.edu/av/altvw103.html#:~:text=Wormholes%20have%20a%20strong%20tendency,matter%22%20objects%20with%20negative%20masses"]
-Text['The Birth of Wormholes'] = ["https://physics.aps.org/story/v15/st11"]
-Text['The Einstein-Rosen Bridge'] = ["https://i4is.org/einstein-rosen-bridge/"]
-Text['A black hole circling a wormhole would emit weird gravitational waves'] = ["https://www.sciencenews.org/article/black-hole-circling-wormhole-weird-gravitational-waves"]
-Text['Singularities'] = ["https://www.physicsoftheuniverse.com/topics_blackholes_singularities.html"]
+Text['Gabe'] = ["https://www.help.hawken.edu", "Gabriel Wolf"]
+Text['The Science of Interstellar'] = ["https://www.amazon.com/Science-Interstellar-Kip-Thorne/dp/0393351378","Kip Thorne"]
+Text['New Improved Wormholes'] = ["www.npl.washington.edu/av/altvw103.html#:~:text=Wormholes%20have%20a%20strong%20tendency,matter%22%20objects%20with%20negative%20masses","woo"]
+Text['The Birth of Wormholes'] = ["https://physics.aps.org/story/v15/st11","woo"]
+Text['The Einstein-Rosen Bridge'] = ["https://i4is.org/einstein-rosen-bridge/","woo"]
+Text['A black hole circling a wormhole would emit weird gravitational waves'] = ["https://www.sciencenews.org/article/black-hole-circling-wormhole-weird-gravitational-waves","woo"]
+Text['Singularities'] = ["https://www.physicsoftheuniverse.com/topics_blackholes_singularities.html","woo"]
